c1e199956b1f6590b6e31e252ed0427f700e9606
Kentaro Hayashi <hayashi@clear-code.com>
2025-03-14 15:49:57 +0900

    podman: fix systemDefaultPolicyPath for termux
    builtinDefaultPolicyPath was used as systemDefaultPolicyPath,
    it should be overridden by EXTRA_LDFLAGS.
    
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- root-packages/podman/build.sh
+++ root-packages/podman/build.sh
@@ -66,7 +66,8 @@ termux_step_make() {
 	# FIXME: need libsubid
 	BUILDTAGS="seccomp selinux libsubid cni exclude_graphdriver_devicemapper exclude_graphdriver_btrfs"
 	#BUILDTAGS="seccomp selinux cni exclude_graphdriver_devicemapper exclude_graphdriver_btrfs"
-	BUILDFLAGS="-x -work" BUILDTAGS="$BUILDTAGS" GOARCH=arm64 make podman
+	EXTRA_LDFLAGS="-X github.com/containers/image/v5/signature.systemDefaultPolicyPath=$TERMUX_PREFIX/etc/containers/policy.json"
+	BUILDFLAGS="-x -work" BUILDTAGS="$BUILDTAGS" GOARCH=arm64 EXTRA_LDFLAGS="$EXTRA_LDFLAGS" make podman
 	# Build podman-remote
 	BUILDFLAGS="-x -work" make podman-remote-static-linux_arm64
 	# Build podman-testing
